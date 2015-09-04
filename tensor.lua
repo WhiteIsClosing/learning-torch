14707bb860676b48d4da72b0cbe07a087c02fe07
alexwchen <alexwchenpro@gmail.com>
2015-09-04 12:13:13 -0700

    learned how to get tensor dim size

--- tensor.lua
+++ tensor.lua
@@ -10,3 +10,12 @@ z = torch.Tensor(4,5,6,2)
 s = torch.LongStorage(6)
 s[1] = 4; s[2] = 5; s[3] = 6; s[4] = 2; s[5] = 7; s[6] = 3;
 x = torch.Tensor(s)
+
+----------------------------------------------------
+-- 1. get nDim, size of a tensor
+----------------------------------------------------
+-- The number of dimensions of a Tensor can be queried by nDimension() or dim().
+-- Size of the i-th dimension is returned by size(i).
+-- A LongStorage containing all the dimensions can be returned by size().
+print (x:nDimension())
+print (x:size())
