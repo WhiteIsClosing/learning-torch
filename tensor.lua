5d6f94deb335a41717eff7834f3b88887c9ff986
alexwchen <alexwchenpro@gmail.com>
2015-09-04 12:29:34 -0700

    learned tensor internal data representation

--- tensor.lua
+++ tensor.lua
@@ -19,3 +19,51 @@ x = torch.Tensor(s)
 -- A LongStorage containing all the dimensions can be returned by size().
 print (x:nDimension())
 print (x:size())
+
+----------------------------------------------------
+-- 2. Internal data representation
+----------------------------------------------------
+-- The actual data of a Tensor is contained into a Storage.
+-- It can be accessed using storage().
+-- While the memory of a Tensor has to be contained in this unique Storage,
+-- it might not be contiguous: the first position used in the Storage is given by storageOffset() (starting at 1).
+-- And the jump needed to go from one element to another element in the i-th dimension is given by stride(i).
+-- In other words, given a 3D tensor
+
+x = torch.Tensor(7,7,7)
+
+-- accessing the element (3,4,5) can be done by
+print(x[3][4][5])
+-- or equivalently (but slowly!)
+-- print(x:storage()[x:storageOffset()+(3-1)*x:stride(1)+(4-1)*x:stride(2)+(5-1)*x:stride(3)])
+-- print(x:storageOffset())
+-- print(x:stride(1))
+-- print(x:stride(2))
+-- print(x:stride(3))
+
+
+
+-- One could say that a Tensor is a particular way of viewing a Storage:
+-- a Storage only represents a chunk of memory,
+-- while the Tensor interprets this chunk of memory as having dimensions:
+x = torch.Tensor(4,5)
+s = x:storage()
+for i=1,s:size() do -- fill up the Storage
+  s[i] = i
+end
+print(x)
+-- print(x:storage())
+
+
+-- Note also that in Torch7 elements in the same row [elements along the last dimension]
+-- are contiguous in memory for a matrix [tensor]:
+
+x = torch.Tensor(4,5)
+i = 0
+
+x:apply(function()
+  i = i + 1
+  return i
+end)
+print(x)
+print(x:stride())
